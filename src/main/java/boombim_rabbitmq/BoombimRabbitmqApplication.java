cddd734d4d180c93b757024ad51fb9a6afe4c778
chltmdgh522 <chltmdgh517@naver.com>
2025-08-28 18:48:33 +0900

    setting init

---
+++ src/main/java/boombim_rabbitmq/BoombimRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package boombim_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoombimRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoombimRabbitmqApplication.class, args);
+	}
+
+}
