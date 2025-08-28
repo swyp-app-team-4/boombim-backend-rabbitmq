f8639b0913bce2cd703c17bda4577a21b2316b2b
chltmdgh522 <chltmdgh517@naver.com>
2025-08-28 18:50:01 +0900

    setting init

---
+++ src/test/java/boombim_rabbitmq/BoombimRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package boombim_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoombimRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
